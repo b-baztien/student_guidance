c93bcc47774236f134326b3f373152efedf300db
devilbas07 <devilbastopoftheworld@gmail.com>
2019-07-05 19:04:28 +0700

    login

--- lib/page/login/login.dart
+++ lib/page/login/login.dart
@@ -2,78 +2,108 @@ import 'package:flutter/material.dart';
 import 'package:student_guidance/page/home/home.dart';
 
 class LoginPage extends StatefulWidget {
-   static String tag = 'login-page';
+  static String tag = 'login-page';
   @override
   _LoginPageState createState() => _LoginPageState();
 }
 
 class _LoginPageState extends State<LoginPage> {
+  loginHeader() => Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          FlutterLogo(
+            colors: Colors.blue,
+            size: 80.0,
+          ),
+          SizedBox(
+            height: 30.0,
+          ),
+          Text(
+            'ระบบจัดการแนะแนวสำหรับนักเรียนมัธยมปลาย',
+            style: TextStyle(fontWeight: FontWeight.w700, color: Colors.blue),
+          ),
+          SizedBox(
+            height: 5.0,
+          ),
+          Text(
+            'ลงชื่อเข้าใช้งาน',
+            style: TextStyle(color: Colors.grey),
+          ),
+        ],
+      );
+
+  loginFields() => Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 30.0),
+              child: TextField(
+                maxLines: 1,
+                decoration: InputDecoration(
+                  hintText: 'กรุณากรอกชื่อผู้ใช้',
+                  labelText: 'ชื่อผู้ใช้',
+                ),
+              ),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 30.0),
+              child: TextField(
+                maxLines: 1,
+                obscureText: true,
+                decoration: InputDecoration(
+                  hintText: 'กรุณากรอกรหัสผ่าน',
+                  labelText: 'รหัสผ่าน',
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 30.0,
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 30.0),
+              width: double.infinity,
+              child: RaisedButton(
+                padding: EdgeInsets.all(15.0),
+                shape: StadiumBorder(),
+                child: Text(
+                  'ลงชื่อเข้าใช้',
+                  style: TextStyle(color: Colors.white),
+                ),
+                color: Colors.blue,
+                onPressed: () {
+                  Navigator.pushNamed(context, Home.tag);
+                },
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Column(children: [
+                Text('มีปัญหาในการเข้าสู่ระบบ ?',
+                    style: TextStyle(color: Colors.grey)),
+              ]),
+            ),
+          ],
+        ),
+      );
+
+  loginBody() => SingleChildScrollView(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: <Widget>[loginHeader(), loginFields()],
+        ),
+      );
 
   @override
   Widget build(BuildContext context) {
-    
-
-    final email = TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      autofocus: false,
-      initialValue: 'alucard@gmail.com',
-      decoration: InputDecoration(
-        hintText: 'Email',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-      ),
-    );
-
-    final password = TextFormField(
-      autofocus: false,
-      initialValue: 'some password',
-      obscureText: true,
-      decoration: InputDecoration(
-        hintText: 'Password',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-      ),
-    );
-
-    final loginButton = Padding(
-      padding: EdgeInsets.symmetric(vertical: 16.0),
-      child: RaisedButton(
-        shape: RoundedRectangleBorder(
-          borderRadius: BorderRadius.circular(24),
-        ),
-        onPressed: () {
-         Navigator.of(context).pushNamed(Home.tag);
-        },
-        padding: EdgeInsets.all(12),
-        color: Colors.lightBlueAccent,
-        child: Text('Log In', style: TextStyle(color: Colors.white)),
-      ),
-    );
-
-    final forgotLabel = FlatButton(
-      child: Text(
-        'Forgot password?',
-        style: TextStyle(color: Colors.black54),
-      ),
-      onPressed: () {},
-    );
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.only(left: 24.0, right: 24.0),
-          children: <Widget>[
-            SizedBox(height: 48.0),
-            email,
-            SizedBox(height: 8.0),
-            password,
-            SizedBox(height: 24.0),
-            loginButton,
-            forgotLabel
-          ],
-        ),
+        child: loginBody()
       ),
     );
   }
