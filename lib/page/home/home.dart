aee2c3cc8f739e943e2b06d03f1089baa8433b27
devilbas07 <devilbastopoftheworld@gmail.com>
2019-07-05 22:44:54 +0700

    home(slivarappbar, staggeredgrid)

--- lib/page/home/home.dart
+++ lib/page/home/home.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:student_guidance/utils/UIdata.dart';
 
 class Home extends StatefulWidget {
-  static String tag = "home-page" ;
+  static String tag = "home-page";
   @override
   _HomeState createState() => _HomeState();
 }
@@ -9,31 +11,61 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    final email = TextFormField(
-      keyboardType: TextInputType.emailAddress,
-      autofocus: false,
-      initialValue: 'alucard@gmail.com',
-      decoration: InputDecoration(
-        hintText: 'Email',
-        contentPadding: EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-        border: OutlineInputBorder(borderRadius: BorderRadius.circular(32.0)),
-      ),
-    );
- return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: ListView(
-          shrinkWrap: true,
-          padding: EdgeInsets.only(left: 24.0, right: 24.0),
-          children: <Widget>[
-            SizedBox(height: 48.0),
-            email,
-            SizedBox(height: 8.0),
-      
-          ],
-        ),
-      ),
-    );
-
+    return Scaffold(
+        backgroundColor: Colors.white,
+        body: NestedScrollView(
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              return <Widget>[
+                SliverAppBar(
+                  expandedHeight: 200.0,
+                  floating: false,
+                  pinned: true,
+                  snap: false,
+                  flexibleSpace: FlexibleSpaceBar(
+                      centerTitle: false,
+                      title: Row(children: <Widget>[
+                        FlutterLogo(
+                          colors: Colors.yellow,
+                          textColor: Colors.white,
+                        ),
+                        SizedBox(
+                          width: 10.0,
+                        ),
+                        Text("Student Guidance",
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 16.0,
+                            ))
+                      ]),
+                      background: Image.network(
+                        "https://images.pexels.com/photos/396547/pexels-photo-396547.jpeg?auto=compress&cs=tinysrgb&h=350",
+                        fit: BoxFit.cover,
+                      )),
+                ),
+              ];
+            },
+            body: Container(
+                child: StaggeredGridView.countBuilder(
+              padding: EdgeInsets.all(5),
+              crossAxisCount: 4,
+              itemCount: UIdata.routesName.length,
+              itemBuilder: (BuildContext context, int index) => Container(
+                    color: Colors.blue,
+                    child: RaisedButton(
+                      child: Text(
+                        UIdata.routesName[index],
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onPressed: () {
+                        Navigator.pushNamed(context, UIdata.routesName[index]);
+                      },
+                    ),
+                  ),
+              staggeredTileBuilder: (int index) =>
+                  new StaggeredTile.count(index % 3 == 0 ? 4 : 2 , 1.5),
+              mainAxisSpacing: 1.0,
+              crossAxisSpacing: 1.0,
+            ))));
   }
 }
