be49a31233c8326c2b8f539be52e9f39dec37278
devilbas07 <38536013+devilbas07@users.noreply.github.com>
2019-07-05 17:11:00 +0700

    Test

--- lib/page/login/login.dart
+++ lib/page/login/login.dart
@@ -97,7 +97,7 @@ class Login extends StatelessWidget {
             GestureDetector(
               onTap: () {},
               child: Column(children: [
-                Text('มีปัญหา',
+                Text('มีปัญหาในการเข้าสู่ระบบ ?',
                     style: TextStyle(color: Colors.grey)),
               ]),
             ),
