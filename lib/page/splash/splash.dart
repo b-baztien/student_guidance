5462c58d539f288fcbaa15a1ded8fb84e2a11bc4
devilbas07 <devilbastopoftheworld@gmail.com>
2019-10-17 23:58:19 +0700

    splash screen

--- lib/page/splash/splash.dart
+++ lib/page/splash/splash.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'package:student_guidance/page/login/landing.dart';
-import 'package:student_guidance/page/login/login.dart';
 
 class SplashPage extends StatefulWidget {
    static String tag = 'splash-page';
@@ -15,7 +14,7 @@ class _SplashPageState extends State<SplashPage> {
     return SplashScreen(
         seconds: 5,
         navigateAfterSeconds: Landing(),
-        imageBackground: NetworkImage('https://viveconstyle.com/wp-content/uploads/2019/03/Brighter-days-quote-mobile-wallpaper.jpg'),
+        imageBackground: AssetImage('assets/images/background.png'),
         backgroundColor: Colors.white,
         styleTextUnderTheLoader: TextStyle(color: Colors.deepPurple),
         photoSize: 100.0,
