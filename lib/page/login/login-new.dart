04f5cafc5e399fbce3961e166954c3b82998bc85
panuwad <panuwad.pan@mtel.co.th>
2020-01-14 10:42:06 +0700

    add feature new-login (redesign feature login)

--- lib/page/login/login-new.dart
+++ lib/page/login/login-new.dart
@@ -7,6 +7,14 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  bool formVisible;
+  int _formsIndex;
+  @override
+  void initState() {
+    super.initState();
+    formVisible = false;
+    _formsIndex = 1;
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,12 +29,13 @@ class _LoginState extends State<Login> {
                 )
             ),
           ),
+
      Align(
          alignment: Alignment.topCenter,
          child: Padding(
            padding: const EdgeInsets.only(top: 50),
            child: Text(
-             'Login',
+             'ล็อคอิน',
              style: TextStyle(
                color: Colors.white,
                fontWeight: FontWeight.w500,
@@ -52,29 +61,247 @@ class _LoginState extends State<Login> {
           ),
           Align(
             alignment: Alignment.bottomCenter,
-           child: Row(
-             children: <Widget>[
-               RaisedButton(
-                 color: Colors.red,
-                 textColor: Colors.white,
-                 elevation: 0,
-                 shape: RoundedRectangleBorder(
-                   borderRadius: BorderRadius.circular(20)
+            child: Padding(
+                padding: EdgeInsets.only(bottom: 100,left: 10,right: 10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+               Expanded(
+                 child: RaisedButton(
+                    color: Colors.blueAccent,
+                   textColor: Colors.white,
+                   child: Text("ลงชื่อเข้าใข้"),
+                   shape: RoundedRectangleBorder(
+                       borderRadius: BorderRadius.circular(20)
+                   ),
+                   onPressed: (){
+                      setState(() {
+                        formVisible = true;
+                        _formsIndex = 1;
+                      });
+                     print("111");
+                   },
                  ),
-                 child: Text("test"),
-                 onPressed: (){
-
-                 },
-               )
-             ],
-           ),
+               ),
+                  const SizedBox(width: 10,),
+                  Expanded(
+                    child: RaisedButton(
+                      color: Colors.grey.shade700,
+                      textColor: Colors.white,
+                      child: Text("สมัครสมาชิก"),
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)
+                      ),
+                      onPressed: (){
+                       setState(() {
+                         formVisible = true;
+                         _formsIndex = 2;
+                       });
+                        print("22");
+                      },
+                    ),
+                  ),
+                ],
+              ),
+            ),
           ),
-    
+
+          AnimatedSwitcher(
+            duration: Duration(milliseconds: 200),
+            child: (!formVisible) ? null : Container(
+              color: Colors.black54,
+              alignment: Alignment.center,
+              child: ListView(
+                shrinkWrap: true,
+                children: <Widget>[
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: <Widget>[
+                          RaisedButton(
+                            color: _formsIndex == 1 ?Colors.blueAccent : Colors.white,
+                            textColor: _formsIndex == 1 ? Colors.white : Colors.black,
+                            child: Text("ลงชื่อเข้าใช้"),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)
+                            ),
+                            onPressed: (){
+                              setState(() {
+                                _formsIndex = 1;
+                              });
+                            },
+                          ),
+                          const SizedBox(width: 10,),
+                          RaisedButton(
+                            color: _formsIndex == 2 ?Colors.blueAccent : Colors.white,
+                            textColor: _formsIndex == 2 ? Colors.white : Colors.black,
+                            child: Text("สมัครสมาชิก"),
+                            shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20)
+                            ),
+                            onPressed: (){
+                              setState(() {
+                                _formsIndex = 2;
+                              });
+                            },
+                          ),
+                          const SizedBox(width: 10,),
+                          IconButton(
+                            color: Colors.white,
+                            icon: Icon(Icons.clear),
+                            onPressed: (){
+                              setState(() {
+                                formVisible = false;
+                              });
+                            },
+                          )
+                        ],
+                      ),
+                      Container(
+                        child: AnimatedSwitcher(
+                          duration: Duration(milliseconds: 300),
+                          child:
+                          _formsIndex == 1 ? LoginForm() : SignupForm(),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+
+
         ],
 
         ),
       )
 
+    );
+  }
+}
+
+class LoginForm extends StatelessWidget {
+  const LoginForm({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.all(16.0),
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+            labelText: "ไอดีผู้ใช้",
+              hasFloatingPlaceholder: true
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+              labelText: "รหัสผ่าน",
+                hasFloatingPlaceholder: true
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          RaisedButton(
+            color: Colors.blueAccent,
+            textColor: Colors.white,
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Text("Login"),
+            onPressed: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SignupForm extends StatelessWidget {
+  const SignupForm({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.0),
+      ),
+      child: ListView(
+        shrinkWrap: true,
+        padding: const EdgeInsets.all(16.0),
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+                labelText: "ชื่อ",
+                hasFloatingPlaceholder: true
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "นามสกุล",
+                hasFloatingPlaceholder: true
+            ),
+          ),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "อีเมล",
+                hasFloatingPlaceholder: true
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          TextField(
+            decoration: InputDecoration(
+                labelText: "ไอดีผู้ใช้",
+                hasFloatingPlaceholder: true
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                labelText: "รหัสผ่าน",
+                hasFloatingPlaceholder: true
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          TextField(
+            obscureText: true,
+            decoration: InputDecoration(
+                labelText: "ยืนยันรหัสผ่าน",
+                hasFloatingPlaceholder: true
+            ),
+          ),
+          const SizedBox(height: 10.0),
+          RaisedButton(
+            color: Colors.blueAccent,
+            textColor: Colors.white,
+            elevation: 0,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+            child: Text("Signup"),
+            onPressed: () {},
+          ),
+        ],
+      ),
     );
   }
 }
