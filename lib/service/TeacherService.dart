5c21e829ef39344d32a18c23b2d72014b784a6b5
devilbas <devilbastopoftheworld@gmail.com>
2020-02-09 02:30:38 +0700

    Update TeacherService.dart

--- lib/service/TeacherService.dart
+++ lib/service/TeacherService.dart
@@ -2,18 +2,6 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:student_guidance/model/Teacher.dart';
 
 class TeacherService {
-  Future<Teacher> getTeacher(DocumentReference doc) async {
-    try {
-      DocumentReference refQuery = doc;
-      Teacher teacher = await refQuery.get().then((doc) async {
-        return Teacher.fromJson(doc.data);
-      });
-      return teacher;
-    } catch (e) {
-      rethrow;
-    }
-  }
-
   Stream<List<Teacher>> getAllTeacherBySchoolName(String schoolName) {
     Query query = Firestore.instance
         .collection('School')
@@ -30,35 +18,31 @@ class TeacherService {
     });
   }
 
-  Stream<List<Teacher>> getAllTeacherBySchoolNameAndPosition(
-      String schoolName, String position) {
-    Query query = Firestore.instance
+  Stream<Map<String, List<Teacher>>> getAllMapTeacherBySchoolName(
+      String schoolName) {
+    Query collectionReference = Firestore.instance
         .collection('School')
         .document(schoolName)
         .collection('Teacher')
-        .where('position', isEqualTo: position)
+        .orderBy('position')
         .orderBy('firstname')
         .orderBy('lastname');
 
-    return query.snapshots().map((snapshot) {
-      return snapshot.documents
-          .map((doc) => Teacher.fromJson(doc.data))
-          .toList();
-    });
-  }
+    return collectionReference.snapshots().map((snapshot) {
+      Map<String, List<Teacher>> mapTeacher = new Map();
+      snapshot.documents.forEach((docChange) {
+        String positionMapKey = docChange.data['position'];
 
-  Stream<List<String>> getAllPositionTeacherBySchoolName(String schoolName) {
-    Query query = Firestore.instance
-        .collection('School')
-        .document(schoolName)
-        .collection('Teacher')
-        .orderBy('position');
+        List<Teacher> listTeacher = new List();
+        snapshot.documents.forEach((doc) {
+          if (positionMapKey == doc.data['position']) {
+            listTeacher.add(Teacher.fromJson(doc.data));
+          }
+        });
+        mapTeacher[positionMapKey] = listTeacher;
+      });
 
-    return query.snapshots().map((snapshot) {
-      return snapshot.documents
-          .map((doc) => Teacher.fromJson(doc.data).position)
-          .toSet()
-          .toList();
+      return mapTeacher;
     });
   }
 }
