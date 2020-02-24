20b2d83e21293911d19ef6ff32926a6ab55abd47
devilbas <devilbastopoftheworld@gmail.com>
2020-02-24 13:51:53 +0700

    change button drawer

--- lib/page/drawer/Mydrawer.dart
+++ lib/page/drawer/Mydrawer.dart
@@ -28,126 +28,124 @@ class _MyDrawerState extends State<MyDrawer> {
       clipper: OvalRighBorderClipper(),
       child: Drawer(
         child: Container(
-          padding: const EdgeInsets.only(left: 16, right: 40),
+          padding: EdgeInsets.only(left: 15, right: 40),
           decoration: BoxDecoration(
               color: Colors.white,
               boxShadow: [BoxShadow(color: Colors.black45)]),
           width: 300,
-          child: SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.centerRight,
-                    child: IconButton(
-                        icon: Icon(
-                          Icons.power_settings_new,
-                          color: Colors.grey.shade800,
+          child: SingleChildScrollView(
+            padding: EdgeInsets.all(0),
+            child: Column(
+              children: <Widget>[
+                Container(
+                  alignment: Alignment.centerRight,
+                  child: IconButton(
+                      icon: Icon(
+                        Icons.power_settings_new,
+                        color: Colors.grey.shade800,
+                      ),
+                      onPressed: () {
+                        LoginService().clearLoginData();
+                        Navigator.pushNamedAndRemoveUntil(
+                            context,
+                            UIdata.loginPageTag,
+                            ModalRoute.withName(UIdata.loginPageTag));
+                      }),
+                ),
+                Container(
+                  height: 90,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      gradient: LinearGradient(
+                          colors: [Colors.orange, Colors.deepOrange])),
+                  child: FutureBuilder(
+                      future: GetImageService().getImage(widget.student.image),
+                      builder: (context, snapshot) {
+                        if (snapshot.hasData) {
+                          return CircleAvatar(
+                            backgroundImage: NetworkImage(snapshot.data),
+                            radius: 40,
+                          );
+                        } else {
+                          return CircleAvatar(
+                            radius: 40,
+                          );
+                        }
+                      }),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  widget.student.firstName + ' ' + widget.student.lastName,
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                      fontFamily: UIdata.fontFamily),
+                ),
+                Text(
+                  widget.schoolId,
+                  style: TextStyle(
+                      color: Colors.blueAccent,
+                      fontSize: 15,
+                      fontFamily: UIdata.fontFamily),
+                ),
+                Text(
+                  widget.student.status,
+                  style: TextStyle(
+                      color: widget.student.status == 'กำลังศึกษา'
+                          ? Colors.green
+                          : Colors.orange,
+                      fontSize: 15,
+                      fontFamily: UIdata.fontFamily),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                _buildRow(
+                  Icons.account_circle,
+                  "แก้ไขข้อมูลส่วนตัว",
+                  Colors.blue,
+                  (context) => EditProfile(
+                    login: widget.login,
+                  ),
+                ),
+                _buildDivider(),
+                widget.student.status == 'กำลังศึกษา'
+                    ? _buildRow(
+                        Icons.add_to_photos,
+                        "เพิ่มข้อมูลการสอบ TCAS",
+                        Colors.green,
+                        (context) => AddEducation(),
+                      )
+                    : _buildRow(
+                        Icons.add_to_photos,
+                        "เพิ่มข้อมูลหลังการจบการศึกษา",
+                        Colors.green,
+                        (context) => EditProfile(
+                          login: widget.login,
                         ),
-                        onPressed: () {
-                          LoginService().clearLoginData();
-                          Navigator.pushNamedAndRemoveUntil(
-                              context,
-                              UIdata.loginPageTag,
-                              ModalRoute.withName(UIdata.loginPageTag));
-                        }),
-                  ),
-                  Container(
-                    height: 90,
-                    alignment: Alignment.center,
-                    decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        gradient: LinearGradient(
-                            colors: [Colors.orange, Colors.deepOrange])),
-                    child: FutureBuilder(
-                        future:
-                            GetImageService().getImage(widget.student.image),
-                        builder: (context, snapshot) {
-                          if (snapshot.hasData) {
-                            return CircleAvatar(
-                              backgroundImage: NetworkImage(snapshot.data),
-                              radius: 40,
-                            );
-                          } else {
-                            return CircleAvatar(
-                              radius: 40,
-                            );
-                          }
-                        }),
-                  ),
-                  SizedBox(
-                    height: 5,
-                  ),
-                  Text(
-                    widget.student.firstName + ' ' + widget.student.lastName,
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 18,
-                        fontWeight: FontWeight.w600,
-                        fontFamily: UIdata.fontFamily),
-                  ),
-                  Text(
-                    widget.schoolId,
-                    style: TextStyle(
-                        color: Colors.blueAccent,
-                        fontSize: 15,
-                        fontFamily: UIdata.fontFamily),
-                  ),
-                  Text(
-                    widget.student.status,
-                    style: TextStyle(
-                        color: widget.student.status == 'กำลังศึกษา'
-                            ? Colors.green
-                            : Colors.orange,
-                        fontSize: 15,
-                        fontFamily: UIdata.fontFamily),
-                  ),
-                  SizedBox(
-                    height: 15,
-                  ),
-                  _buildRow(
-                    Icons.account_circle,
-                    "แก้ไขข้อมูลส่วนตัว",
-                    Colors.blue,
+                      ),
+                _buildDivider(),
+                _buildRow(
+                    Icons.vpn_key,
+                    "เปลี่ยนพาสเวิร์ด",
+                    Colors.yellow,
                     (context) => EditProfile(
-                      login: widget.login,
-                    ),
-                  ),
-                  _buildDivider(),
-                  widget.student.status == 'กำลังศึกษา'
-                      ? _buildRow(
-                          Icons.add_to_photos,
-                          "เพิ่มข้อมูลการสอบ TCAS",
-                          Colors.green,
-                          (context) => AddEducation(),
-                        )
-                      : _buildRow(
-                          Icons.add_to_photos,
-                          "เพิ่มข้อมูลหลังการจบการศึกษา",
-                          Colors.green,
-                          (context) => EditProfile(
-                            login: widget.login,
-                          ),
-                        ),
-                  _buildDivider(),
-                  _buildRow(
-                      Icons.vpn_key,
-                      "เปลี่ยนพาสเวิร์ด",
-                      Colors.yellow,
-                      (context) => EditProfile(
-                            login: widget.login,
-                          )),
-                  _buildDivider(),
-                  _buildRow(
-                      Icons.favorite,
-                      "สาขาที่ติดตาม",
-                      Colors.red[300],
-                      (context) => EditProfile(
-                            login: widget.login,
-                          )),
-                  _buildDivider(),
-                ],
-              ),
+                          login: widget.login,
+                        )),
+                _buildDivider(),
+                _buildRow(
+                    Icons.favorite,
+                    "สาขาที่ติดตาม",
+                    Colors.red[300],
+                    (context) => EditProfile(
+                          login: widget.login,
+                        )),
+                _buildDivider(),
+              ],
             ),
           ),
         ),
@@ -165,15 +163,16 @@ class _MyDrawerState extends State<MyDrawer> {
       Function(BuildContext) functionPageBuilder) {
     final TextStyle textStyle =
         TextStyle(color: Colors.black, fontFamily: 'Kanit', fontSize: 15);
-    return GestureDetector(
-      onTap: () {
+    return FlatButton(
+      padding: EdgeInsets.all(0),
+      onPressed: () {
         Navigator.push(
           context,
           MaterialPageRoute(builder: functionPageBuilder),
         );
       },
       child: Container(
-        padding: const EdgeInsets.symmetric(vertical: 5),
+        padding: EdgeInsets.symmetric(vertical: 0),
         child: Row(
           children: <Widget>[
             Icon(icon, color: colors),
