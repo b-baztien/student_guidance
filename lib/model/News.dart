6c4d61f7c83802e4535273bee91648f968b3a778
devilbas <devilbastopoftheworld@gmail.com>
2020-02-24 14:16:44 +0700

    Update News.dart

--- lib/model/News.dart
+++ lib/model/News.dart
@@ -6,20 +6,35 @@ class News {
   String image;
   Timestamp startTime;
   List listUniversityName;
+  String teacherName;
 
   News(
       {this.detail,
       this.image,
       this.topic,
       this.startTime,
-      this.listUniversityName});
+      this.listUniversityName,
+      this.teacherName});
+
   factory News.fromJson(Map<String, dynamic> json) {
     return News(
       topic: json['topic'] as String,
       detail: json['detail'] as String,
       image: json['image'] as String,
       startTime: json['start_time'] as Timestamp,
-      listUniversityName: json['listUniversity_name'],
+      listUniversityName: json['listUniversity_name'] as List,
+      teacherName: json['teacherName'],
     );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'topic': topic,
+      'detail': detail,
+      'image': image,
+      'start_time': startTime,
+      'listUniversity_name': listUniversityName,
+      'teacherName': teacherName,
+    };
   }
 }
