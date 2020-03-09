6ddcf753e1a49f436b4bc1c0112ebfec9db1861e
devilbas <devilbastopoftheworld@gmail.com>
2020-03-09 11:53:32 +0700

    Update CareerService.dart

--- lib/service/CareerService.dart
+++ lib/service/CareerService.dart
@@ -1,12 +1,12 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class CareerService {
-  Future<QuerySnapshot> getCareerByCarrerName(String name) async {
+  Future<DocumentSnapshot> getCareerByCarrerName(String name) async {
     CollectionReference collectionReference =
         Firestore.instance.collection('Career');
     QuerySnapshot qs = await collectionReference
         .where('career_name', isEqualTo: name)
         .getDocuments();
-    return qs;
+    return qs.documents[0];
   }
 }
