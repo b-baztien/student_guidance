35c199e6cc89a21e363f0b85d9991be1c2bd78da
devilbas <devilbastopoftheworld@gmail.com>
2020-03-23 22:04:01 +0700

    Update DashboardService.dart

--- lib/service/DashboardService.dart
+++ lib/service/DashboardService.dart
@@ -4,12 +4,11 @@ import 'package:student_guidance/model/Alumni.dart';
 import 'package:student_guidance/model/DashboardAlumni.dart';
 
 class DashboardService {
-  Stream<List<DashboardAlumni>> getAlumniDashboard(
-      String schoolName, String year) {
+  Stream<DashboardAlumni> getAlumniDashboard(String schoolName) {
     Stream<QuerySnapshot> alumniSnapshot = Firestore.instance
         .collectionGroup('Alumni')
         .where('schoolName', isEqualTo: schoolName)
-        .where('graduate_year', isEqualTo: year)
+        .orderBy('graduate_year')
         .snapshots();
     Stream<QuerySnapshot> entranceMajorSnapshot =
         Firestore.instance.collectionGroup('EntranceMajor').snapshots();
@@ -23,34 +22,26 @@ class DashboardService {
 
       total = alumniData.documents.length;
 
-      Set<String> listYear = alumniData.documents
-          .map((alumniDoc) => Alumni.fromJson(alumniDoc.data).graduateYear)
-          .toSet();
-      List<DashboardAlumni> listDashboardAlumni = List();
+      String year = Alumni.fromJson(alumniData.documents[0].data).graduateYear;
 
-      for (var year in listYear) {
-        DashboardAlumni dashboardAlumni;
-        for (var alumniDoc in alumniData.documents) {
-          String graduateDateTime =
-              Alumni.fromJson(alumniDoc.data).graduateYear;
+      DashboardAlumni dashboardAlumni;
+      for (var alumniDoc in alumniData.documents) {
+        String graduateDateTime = Alumni.fromJson(alumniDoc.data).graduateYear;
 
-          if (year == graduateDateTime) {
-            if (Alumni.fromJson(alumniDoc.data).status == 'ศึกษาต่อ') {
-              studying++;
-            } else if (Alumni.fromJson(alumniDoc.data).status == 'ไม่ระบุ') {
-              other++;
-            } else {
-              working++;
-            }
+        if (year == graduateDateTime) {
+          if (Alumni.fromJson(alumniDoc.data).status == 'ศึกษาต่อ') {
+            studying++;
+          } else if (Alumni.fromJson(alumniDoc.data).status == 'ไม่ระบุ') {
+            other++;
+          } else {
+            working++;
           }
         }
-
-        dashboardAlumni =
-            DashboardAlumni(year, total, studying, working, other);
-        listDashboardAlumni.add(dashboardAlumni);
       }
 
-      return listDashboardAlumni.take(5).toList();
+      dashboardAlumni = DashboardAlumni(year, total, studying, working, other);
+
+      return dashboardAlumni;
     });
   }
 
