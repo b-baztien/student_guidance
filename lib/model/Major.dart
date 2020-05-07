f4415dd2c587ea1cc589446d28c601f2eeaed48c
devilbas <devilbastopoftheworld@gmail.com>
2020-05-08 01:44:30 +0700

    remove tcas in major class

--- lib/model/Major.dart
+++ lib/model/Major.dart
@@ -1,27 +1,6 @@
-import 'dart:collection';
-
-class Tcas {
-  String round;
-  String description;
-  List<String> examReference;
-
-  Tcas({this.round, this.description, this.examReference});
-
-  factory Tcas.fromJson(LinkedHashMap json) {
-    return Tcas(
-      round: json['round'] as String,
-      description: json['description'] as String,
-      examReference: (json['examReference'] as List)
-          .map((data) => data as String)
-          .toList(),
-    );
-  }
-}
-
 class Major {
   String majorName;
   String url;
-  List<Tcas> tcasEntranceRound;
   String certificate;
   List<String> listCareerName;
   String courseDuration;
@@ -30,7 +9,6 @@ class Major {
   Major({
     this.majorName,
     this.url,
-    this.tcasEntranceRound,
     this.listCareerName,
     this.certificate,
     this.courseDuration,
